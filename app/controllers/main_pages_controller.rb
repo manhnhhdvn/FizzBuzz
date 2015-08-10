a11f272f803b1c300014907cf51b977e14d134e4
manhnhhedspi <manhnhhedspi@gmail.com>
2015-08-10 13:11:48 +0700

    Finish FizzBuzz text

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,10 +1,38 @@
 class MainPagesController < ApplicationController
+  require "will_paginate/array"
+
   def home
   end
 
-  def fizzbuzz_text
+  def fizzbuzztext
+    result
   end
 
-  def fizzbuzz_image
+  def fizzbuzzimage
+  	result
+  end
+
+  private
+  def fizz? number
+    true if number%3 == 0
+  end
+
+  def buzz? number
+    true if number%5 == 0
+  end
+
+  def result
+    array = []
+    1000.times{|number|
+      if fizz? number + 1
+        array << "Fizz"
+        array << "Buzz" if buzz? number + 1
+      elsif buzz? number + 1
+        array << "Buzz"
+      else
+        array << number + 1
+      end
+    }
+    @main_pages = array.paginate page: params[:page], per_page: 10
   end
 end
